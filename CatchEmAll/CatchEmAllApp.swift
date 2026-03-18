a1033569c0ed3db4a6bb433f6b69c35b19c3c5b7
Sige Wang <wangfiz@bc.edu>
2026-03-17 21:18:50 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Wang Sige on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
